e1b23f9c743ca5e328f36db80d4fbdb2b74c42be
Christopher <obinna.h@outlook.com>
2025-07-14 14:48:26 +0100

    Player Creation
    Created player and altered player movement script for core game functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,65 +8,30 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Player Movement")]
     [SerializeField] Rigidbody2D playerRb2D;
-    [SerializeField] float playerMoveSpeed = 20.0f;
     [SerializeField] float playerJumpHeight = 10.0f;
 
     [Header("Player Collisions")]
     [SerializeField] Collider2D playerCollider2D;
     [SerializeField] LayerMask obstacleLayers;
 
-    [Header("Player Camera Bounds")]
-    [SerializeField] float paddingLeft;
-    [SerializeField] float paddingRight;
-    [SerializeField] float paddingBottom;
-    [SerializeField] float paddingTop;
 
     int numOfJumps = 2;
     bool isGrounded;
     bool canDoubleJump = false;
 
-    //Vector2 moveVelocity;
-    Vector2 playerRawInput;
-
-    private Vector2 minBounds;
-    private Vector2 maxBounds;
 
     private void Awake()
     {
-        playerCollider2D = GetComponent<Collider2D>();
-        playerRb2D = GetComponent<Rigidbody2D>();
-    }
-
-    private void Start()
-    {
-        //InitBounds();
+        playerCollider2D = GetComponentInChildren<Collider2D>();
+        playerRb2D = GetComponentInChildren<Rigidbody2D>();
     }
 
     void Update()
     {
 
-        //moveVelocity = new Vector2(1.0f * playerMoveSpeed, playerRb2D.velocity.y);
-        //playerRb2D.velocity = moveVelocity;
-
-
-        //Vector2 newClampedPosVector = new Vector2();
-
-        //newClampedPosVector.x = Mathf.Clamp(transform.position.x + moveVector.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
-        //newClampedPosVector.y = Mathf.Clamp(transform.position.y + moveVector.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
-
-        //playerRb2D.velocity = new Vector2(playerRawInput.x * playerMoveSpeed * Time.deltaTime,playerRb2D.velocity.y);
-
-        
         GroundedCheck();
         Damage();
     }
-
-    //void InitBounds()
-    //{
-    //    Camera mainCamera = Camera.main;
-    //    minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
-    //    maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
-    //}
 
     void OnJump(InputValue value)
     {
@@ -84,12 +49,6 @@ public class PlayerMovement : MonoBehaviour
                 playerRb2D.velocity += new Vector2(0.0f, playerJumpHeight);
             }
         }
-    }
-
-    private void OnMove(InputValue value)
-    {
-        playerRawInput = value.Get<Vector2>();
-        Debug.Log(playerRawInput);    
     }
 
 
