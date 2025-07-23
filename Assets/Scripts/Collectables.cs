62c69660273a1562ce99523b5596890a95d7041c
Christopher <obinna.h@outlook.com>
2025-07-23 19:47:02 +0100

    PlayerHealth and Obstacle Destruction
    Added a collider to destroy obstacles, and connected player health icons to the players health.

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+
+    //value of collectable
+    //speed of collectable
+    
+
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    private int playerHealth = 3;
+
+    private const int maxPlayerHealth = 3;
+
+    private int playerHealth = maxPlayerHealth;
+
+    [SerializeField]
+    private Sprite healthSprite;
+
+    [SerializeField]
+    private Image[] healthIcons = new Image[maxPlayerHealth];
+
+
     private bool isAlive = true;
 
     private void Update()
     {
+        CheckAliveStatus();
+
+    }
+
+    private void CheckAliveStatus()
+    {
         if (playerHealth <= 0 && isAlive)
-        {            
+        {
             isAlive = false;
         }
 
@@ -21,7 +40,6 @@ public class PlayerHealth : MonoBehaviour
 
             Debug.Log("Dead");
         }
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,8 +47,15 @@ public class PlayerHealth : MonoBehaviour
         if (collision.tag == "Obstacle" && isAlive)
         {
             playerHealth--;
+            UpdateHealthIcons();
             Debug.Log(playerHealth);
         }
     }
 
+    private void UpdateHealthIcons()
+    {
+
+        healthIcons[playerHealth].sprite = healthSprite;
+
+    }
 }
