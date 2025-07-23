4f4341a0df328f9b0f39d42c7de246d9fa1c0790
Christopher <obinna.h@outlook.com>
2025-07-23 19:10:02 +0100

    Fast Fall
    implemented a fast fall mechanic and adjusted sprites on obstacle prefabs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TextCore.Text;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,59 +9,96 @@ public class PlayerMovement : MonoBehaviour
 {
 
     [Header("Player Movement")]
-    [SerializeField] Rigidbody2D playerRb2D;
-    [SerializeField] float playerJumpHeight = 10.0f;
+    [SerializeField] private Rigidbody2D playerRb2D;
+    [SerializeField] private float playerJumpHeight = 10.0f;
+    [SerializeField] private float playerUpscaledGravity = 15.0f;
 
     [Header("Player Collisions")]
     [SerializeField] Collider2D playerCollider2D;
     [SerializeField] LayerMask obstacleLayers;
 
+    private PlayerInput playerInputActions;
 
-    bool isGrounded;
-    bool canDoubleJump = false;
-
+    private float playerDefaultGravityScale;
+    private bool isGrounded;
+    private bool canDoubleJump = false;
 
     private void Awake()
     {
         playerCollider2D = GetComponentInChildren<Collider2D>();
         playerRb2D = GetComponentInChildren<Rigidbody2D>();
+
+        playerInputActions = new PlayerInput();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += OnJump;
+        playerInputActions.Player.Fall.performed += OnFastFall;
+
     }
 
-    void Update()
+    private void Start()
     {
+        playerDefaultGravityScale = playerRb2D.gravityScale;
+    }
 
+    private void Update()
+    {
         GroundedCheck();
     }
 
-    void OnJump(InputValue value)
+    #region Jump
+
+    private void OnJump(InputAction.CallbackContext context)
     {
-        if (value.isPressed)
+
+        ProcessJump();
+    }
+
+    private void ProcessJump()
+    {
+
+        if (isGrounded)
         {
-            if (isGrounded)
-            {
-                playerRb2D.velocity = new Vector2(playerRb2D.velocity.x, 0);
-                playerRb2D.velocity += new Vector2(0.0f,playerJumpHeight);
-                canDoubleJump = true;
-            }else if (canDoubleJump)
-            {
-                canDoubleJump = false;
-                playerRb2D.velocity = new Vector2(playerRb2D.velocity.x, 0);
-                playerRb2D.velocity += new Vector2(0.0f, playerJumpHeight);
-            }
+            playerRb2D.velocity = new Vector2(playerRb2D.velocity.x, 0);
+            playerRb2D.velocity += new Vector2(0.0f, playerJumpHeight);
+            canDoubleJump = true;
+        }
+        else if (canDoubleJump)
+        {
+            canDoubleJump = false;
+            playerRb2D.velocity = new Vector2(playerRb2D.velocity.x, 0);
+            playerRb2D.velocity += new Vector2(0.0f, playerJumpHeight*0.75f);
         }
     }
 
-
-    void GroundedCheck()
+    private void GroundedCheck()
     {
         if (playerCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             isGrounded = true;
+            playerRb2D.gravityScale = playerDefaultGravityScale;
         }
         else
         {
             isGrounded = false;
         }
     }
+
+    #endregion
+
+
+    #region Fast Fall
+
+    private void OnFastFall(InputAction.CallbackContext context)
+    {
+        if (!isGrounded)
+        {
+            playerRb2D.gravityScale = playerUpscaledGravity;
+            canDoubleJump = false;
+        }
+    }
+
+
+
+    #endregion
 
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -15,18 +16,28 @@ public class Spawner : MonoBehaviour
     private void Update()
     {
 
+        SpawnLoop();
+
+
+    }
+
+    private void SpawnLoop()
+    {
+
         timer += Time.deltaTime;
 
         if (timer >= intervalTime)
         {
-            int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
-
-            Instantiate(obstaclePrefabs[obstacleIndex], transform.position, Quaternion.identity);
-
-            timer = 0.0f; 
+            SpawnObstacle();     
+            timer = 0.0f;
         }
-
     }
 
+    private void SpawnObstacle()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+
+        GameObject obstacleToSpawn = Instantiate(obstaclePrefabs[obstacleIndex], transform.position, Quaternion.identity);
+    }
 
 }
